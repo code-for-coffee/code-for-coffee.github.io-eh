df32a330b4920059e098cc17db81b79abdc5c0f6
James <jtraverjr@gmail.com>
2014-05-10 02:35:22 -0500

    meta model

---
+++ js/models/app.Models.Content.Meta.js
@@ -0,0 +1,8 @@
+app.Models.Content.Meta = Backbone.Model.extend({
+    defaults: {
+        name: "codeforcoffee.org",
+        header: "Software Engineering and Coffee",
+        author: "James",
+        contactEmail: "james@codeforcoffee.org"
+    }
+});
